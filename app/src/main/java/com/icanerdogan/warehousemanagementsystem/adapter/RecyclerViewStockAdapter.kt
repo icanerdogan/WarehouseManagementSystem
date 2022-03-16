fa6a26ec770bad0fde70ca2fe2618f66613391ce
icanerdogan <ibrahim4545erdogan@gmail.com>
2022-03-16 14:48:36 +0300

    RecyclerViewClicked

--- app/src/main/java/com/icanerdogan/warehousemanagementsystem/adapter/RecyclerViewStockAdapter.kt
+++ app/src/main/java/com/icanerdogan/warehousemanagementsystem/adapter/RecyclerViewStockAdapter.kt
@@ -1,25 +1,61 @@
 package com.icanerdogan.warehousemanagementsystem.adapter
 
+import android.R.attr.label
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
-import com.icanerdogan.warehousemanagementsystem.R
 import com.icanerdogan.warehousemanagementsystem.databinding.StockRowLayoutBinding
 import com.icanerdogan.warehousemanagementsystem.model.Product
 
-class RecyclerViewStockAdapter(private val productList : ArrayList<Product>) : RecyclerView.Adapter<RecyclerViewStockAdapter.RowHolder>() {
 
-    class RowHolder(val stockRowLayoutBinding: StockRowLayoutBinding) : RecyclerView.ViewHolder(stockRowLayoutBinding.root)
+class RecyclerViewStockAdapter(private val productList: ArrayList<Product>) :
+    RecyclerView.Adapter<RecyclerViewStockAdapter.RowHolder>() {
+    private lateinit var clipboard: ClipboardManager
+    private lateinit var clip: ClipData
+
+    class RowHolder(val stockRowLayoutBinding: StockRowLayoutBinding) :
+        RecyclerView.ViewHolder(stockRowLayoutBinding.root)
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowHolder {
-        return RowHolder(StockRowLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return RowHolder(
+            StockRowLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
+
 
     override fun onBindViewHolder(holder: RowHolder, position: Int) {
         holder.stockRowLayoutBinding.stockproduct = productList[position]
+
+        holder.itemView.apply {
+
+            setOnClickListener {
+                Toast.makeText(
+                    this.context,
+                    "Barcode Number: ${holder.stockRowLayoutBinding.textViewProductBarcodeNumber.text}",
+                    Toast.LENGTH_LONG
+                ).show()
+
+            }
+            setOnLongClickListener{
+                val text = holder.stockRowLayoutBinding.textViewProductBarcodeNumber.text.toString()
+                clipboard = it.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                clip = ClipData.newPlainText("BarcodeNumber", text)
+                clipboard.setPrimaryClip(clip)
+                Toast.makeText(it.context, "Barkod Numarası Kopyalandı!", Toast.LENGTH_SHORT).show();
+                return@setOnLongClickListener true
+            }
+        }
     }
 
     override fun getItemCount(): Int {
